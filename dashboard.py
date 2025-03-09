d9f577345c2abe245597b9ceb5502ebb2e1b4195
unknown <sukmoguruh7@gmail.com>
2025-03-10 00:22:23 +0700

    fxx

--- dashboard.py
+++ dashboard.py
@@ -63,7 +63,7 @@ with tab3:
     st.header("Effect of weather on bike rental")
     fig,ax = plt.subplots()
     sns.boxplot(x="weather_situation",y="total",data=hour,ax=ax)
-    ax.set_xticks(range(1,4))   
+    ax.set_xticks(range(1,5))   
     ax.set_xticklabels(['Clear','Mist','Light Snow','Heavy Rain'])
     st.pyplot(fig)
 
